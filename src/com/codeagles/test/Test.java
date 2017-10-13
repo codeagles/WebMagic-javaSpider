0251174173a18de67ec6f7e4749b9f59a456f92b
codeagles <hcn0521@outlook.com>
2017-10-13 19:14:58 +0800

    修改

--- src/com/codeagles/test/Test.java
+++ src/com/codeagles/test/Test.java
@@ -1,22 +1,17 @@
 package com.codeagles.test;
 
+import java.text.NumberFormat;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import redis.clients.jedis.Jedis;
+
 public class Test {
-	 public static void testSelenium() {
-	        System.getProperties().setProperty("webdriver.chrome.driver", "/Users/codeagles/ProgramTools/chromedriver2");
-	        WebDriver webDriver = new ChromeDriver();
-	        webDriver.get("http://www.jianshu.com/u/bbfef3982813");
-	        WebElement webElement = webDriver.findElement(By.xpath("/html"));
-	        System.out.println(webElement.getAttribute("outerHTML"));
-	        webDriver.close();
-	    }
-	 public static void main(String[] args) {
-		 testSelenium();
-	 }
-	 
-	 
+	public static void main(String[] args) {
+	   
+		
+	}
 }
